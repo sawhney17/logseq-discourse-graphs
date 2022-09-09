bb05cd80f416b2236c3a7ea007e42817fe2519aa
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-09-09 09:29:37 +0400

    enhance(context): Improve discourse context

--- src/queryFormat.tsx
+++ src/queryFormat.tsx
@@ -1,52 +1,65 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from "react";
 import { render } from "react-dom";
-import { returnedQuery, returnQueries } from './utils';
-const QueryFormat = () =>{
-  const [sections, setSections] = useState([])
+import { returnedQuery, returnQueries } from "./utils";
+const QueryFormat = () => {
+  const [sections, setSections] = useState([]);
   const [responses, setResponses] = useState(null);
-
 
   useEffect(() => {
     returnQueries().then((queries) => {
-      setSections(queries)
-    })
-    }, [])
+      setSections(queries);
+    });
+  }, []);
 
   return (
-    <div className='grid grid-cols-4'>
-      <div className='col-span-1 w-full h-full bg-black'>
-        <div className="">
-          {
-             sections.map((section, index) => {
-              if (section.value.length != 0){
-              console.log(sections)
-              return (
-                <div className='p-2'>
-                  <button onClick={() => { setResponses(index) }}>{section.connection}</button>
-                </div>
-              )
-            }
-            })
-          }
-          {/* {console.log(sections2)}
-          {console.log("sections2")} */}
-          {/* <label>{somethignselse}</label> */}
-        </div>
-      </div>
-      <div className='col-span-3'>
-        <div>
-          {
-            responses == null ? "Check out Discourse Connections here!" :
-            sections[responses].value.map(
-              (response, index) => { 
-                return <button onClick={()=>{logseq.App.pushState('page', response)}} className='block'>{response.name}</button> 
-              })
-          }
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <td>Connection</td>
+          <td>Page</td>
+        </tr>
+        </thead>
+      
+        {sections.map((section, index) => {
+          if (section.value.length != 0) {
+            console.log(sections);
+            return (
+              // <div>
+              <tr>
+                <td>
+                  <button
+                    onClick={() => {
+                      setResponses(index);
+                    }}
+                  >
+                    {section.connection}
+                  </button>
+                </td>
 
-        </div>
-      </div>
+                {index == null
+                  ? "Check out Discourse Connections here!"
+                  : sections[index].value.map((response, index) => {
+                      return (
+                        <button
+                          onClick={() => {
+                            logseq.App.pushState("page", response);
+                          }}
+                          className="block"
+                        >
+                          <a>{response.name}</a>
+                        </button>
+                      );
+                    })}
+              </tr>
+              //</div>
+            );
+          }
+        })}
+      </table>
+
     </div>
-  )
+  );
 };
 
 export default QueryFormat;
